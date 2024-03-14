a8a5ea2dd22dd043f2606d8332d9cd437eb92360
Daconus <108086940+Daconus@users.noreply.github.com>
2024-03-14 12:19:36 -0600

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -3,25 +3,6 @@ namespace myImages {
 
     helpers._registerFactory("image", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "Ls{R8$^5K=X8OQ}dCd-?":
-            case "proj_basic":return img`
-. 2 2 2 . 
-2 4 2 4 2 
-4 4 4 2 4 
-4 2 4 4 4 
-2 4 4 2 2 
-2 2 4 2 2 
-. 2 2 2 . 
-. 4 2 . . 
-. 4 4 4 . 
-2 . . 2 . 
-. 2 4 . 2 
-. . 4 2 . 
-. 4 4 4 . 
-2 . 2 . . 
-. 2 . . . 
-. . . 4 . 
-`;
             case "image1":
             case "myImage":return img`
 . . . . . f . . . . . 
@@ -44,6 +25,126 @@ f 2 2 f d d d f 2 2 f
 . . . . . . . . . . . 
 . . . . . 2 . . . . . 
 . . . . . . . . . . . 
+`;
+            case "image3":
+            case "proj_trishot":return img`
+. . 2 2 2 . . 
+. 2 4 4 4 2 . 
+2 4 4 5 4 4 2 
+2 4 5 5 5 4 2 
+2 4 4 5 4 4 2 
+. 2 4 4 4 2 . 
+. . 2 2 2 . . 
+`;
+            case "(M=;!C3LUZOT|ouSRI?k":
+            case "player_ship":return img`
+. . f f . . . . . . . . . f f . . 
+. f 4 1 f . . . . . . . f 1 4 f . 
+f 1 4 d f . . . . . . . f d 4 1 f 
+f d 4 d f . . . . . . . f d 4 d f 
+f d 4 d f . f f f f f . f d 4 d f 
+f d 4 d f f 1 6 6 6 1 f f d 4 d f 
+f 4 d d f 1 d d d d d 1 f d d 4 f 
+f 4 d d f d d 4 4 4 d d f d d 4 f 
+f 4 d d f d d 4 4 4 d d f d d 4 f 
+f 4 d d f b d d 4 d d b f d d 4 f 
+f 4 d d f f b b b b b f f d d 4 f 
+. f b b f . f f f f f . f b b f . 
+. . f f . . . . . . . . . f f . . 
+`;
+            case "image4":
+            case "proj_beam":return img`
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.424.
+.42..
+`;
+            case "Ls{R8$^5K=X8OQ}dCd-?":
+            case "proj_basic":return img`
+. 2 . 
+2 5 2 
+2 5 2 
+2 5 2 
+2 5 2 
+2 5 2 
+. 2 . 
+`;
+            case "image2":
+            case "proj_plasma":return img`
+. . 8 8 8 . . 
+. 8 9 9 9 8 . 
+8 9 9 9 9 9 8 
+8 9 9 9 9 9 8 
+8 9 9 9 9 9 8 
+8 9 9 9 9 9 8 
+8 9 9 9 9 9 8 
+. 8 9 9 9 8 . 
+. 8 9 9 9 8 . 
+. 8 9 9 9 8 . 
+. . 8 9 8 . . 
+. . 8 9 8 . . 
+. . . 8 . . . 
+. . . 8 . . . 
 `;
         }
         return null;
