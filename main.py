13152e78997d5e76a28914acc6fcc44565b9c4c0
Daconus <108086940+Daconus@users.noreply.github.com>
2024-03-18 12:29:12 -0600

    Update pxt.json, main.blocks, main.ts, main.py, images.g.jres, images.g.ts

--- main.py
+++ main.py
@@ -1,51 +1,45 @@
+@namespace
+class SpriteKind:
+    UI = SpriteKind.create()
+    enemy_Proj = SpriteKind.create()
+
+def on_b_pressed():
+    global playerWeapon, currentWeapon
+    if weapon_1 == playerWeapon:
+        playerWeapon = weapon_2
+        currentWeapon = 2
+    else:
+        playerWeapon = weapon_1
+        currentWeapon = 1
+controller.B.on_event(ControllerButtonEvent.PRESSED, on_b_pressed)
+
+def on_on_overlap(sprite, otherSprite):
+    sprites.destroy(sprite)
+    if info.life() < 0:
+        playLongSFX(lsfx_player_hit, 0)
+        info.change_life_by(-1)
+    else:
+        sprites.destroy(otherSprite, effects.disintegrate, 500)
+        music.play(sfx_death, music.PlaybackMode.UNTIL_DONE)
+        game.game_over(False)
+sprites.on_overlap(SpriteKind.enemy_Proj, SpriteKind.player, on_on_overlap)
+
 # This function takes a long sound effect and plays it
 def playLongSFX(lsfx: List[music.Playable], delay: number):
     for sound in lsfx:
         music.play(sound, music.PlaybackMode.UNTIL_DONE)
         pause(delay)
-# sound for basic shot
-sfx_shoot_basic = music.create_sound_effect(WaveShape.SQUARE,
-    3648,
-    300,
-    255,
-    129,
-    150,
-    SoundExpressionEffect.NONE,
-    InterpolationCurve.LOGARITHMIC)
-# sound for missile shot
-sfx_shoot_missile = music.create_sound_effect(WaveShape.NOISE,
-    2430,
-    958,
-    255,
-    113,
-    300,
-    SoundExpressionEffect.VIBRATO,
-    InterpolationCurve.LOGARITHMIC)
-# sound for salvo shot
-lsfx_shoot_salvo = [music.create_sound_effect(WaveShape.SAWTOOTH,
-        2430,
-        958,
-        255,
-        113,
-        75,
-        SoundExpressionEffect.VIBRATO,
-        InterpolationCurve.LOGARITHMIC),
-    music.create_sound_effect(WaveShape.SAWTOOTH,
-        2430,
-        958,
-        255,
-        113,
-        75,
-        SoundExpressionEffect.VIBRATO,
-        InterpolationCurve.LOGARITHMIC),
-    music.create_sound_effect(WaveShape.SAWTOOTH,
-        2430,
-        958,
-        255,
-        113,
-        75,
-        SoundExpressionEffect.VIBRATO,
-        InterpolationCurve.LOGARITHMIC)]
+proj_plasma: Sprite = None
+proj_missile: Sprite = None
+proj_trishot: Sprite = None
+proj_basic: Sprite = None
+currentWeapon = 0
+playerWeapon = 0
+weapon_2 = 0
+weapon_1 = 0
+sfx_death: music.SoundEffect = None
+lsfx_player_hit: List[music.SoundEffect] = []
+effects.star_field.start_screen_effect()
 # sound for weaker hits
 lsfx_basic_hit = [music.create_sound_effect(WaveShape.NOISE,
         3856,
@@ -88,6 +82,42 @@ lsfx_player_hit = [music.create_sound_effect(WaveShape.NOISE,
         300,
         SoundExpressionEffect.TREMOLO,
         InterpolationCurve.LINEAR)]
+# sound for basic shot
+sfx_shoot_basic = music.create_sound_effect(WaveShape.SQUARE,
+    3648,
+    300,
+    255,
+    129,
+    150,
+    SoundExpressionEffect.NONE,
+    InterpolationCurve.LOGARITHMIC)
+# sound for missile shot
+sfx_shoot_missile = music.create_sound_effect(WaveShape.NOISE,
+    2430,
+    958,
+    255,
+    55,
+    300,
+    SoundExpressionEffect.VIBRATO,
+    InterpolationCurve.LOGARITHMIC)
+# sound for missile shot
+sfx_shoot_plasma = music.create_sound_effect(WaveShape.SQUARE,
+    1463,
+    1,
+    255,
+    50,
+    400,
+    SoundExpressionEffect.WARBLE,
+    InterpolationCurve.CURVE)
+# sound for missile shot
+sfx_death = music.create_sound_effect(WaveShape.SQUARE,
+    4299,
+    1137,
+    255,
+    50,
+    1000,
+    SoundExpressionEffect.WARBLE,
+    InterpolationCurve.CURVE)
 # sound for player heal
 sfx_heal = music.create_sound_effect(WaveShape.SINE,
     1,
@@ -97,30 +127,137 @@ sfx_heal = music.create_sound_effect(WaveShape.SINE,
     200,
     SoundExpressionEffect.NONE,
     InterpolationCurve.CURVE)
-sfx_missile_explosion = music.create_sound_effect(WaveShape.NOISE,
-    2474,
-    1137,
+sfx_plasma_explosion = music.create_sound_effect(WaveShape.SQUARE,
+    1463,
+    1,
     255,
-    108,
+    17,
     500,
+    SoundExpressionEffect.WARBLE,
+    InterpolationCurve.CURVE)
+sfx_missile_explosion = music.create_sound_effect(WaveShape.NOISE,
+    1538,
+    379,
+    255,
+    0,
+    250,
     SoundExpressionEffect.TREMOLO,
-    InterpolationCurve.LOGARITHMIC)
-mySprite = sprites.create(img("""
-        . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . .
-    """),
-    SpriteKind.player)
+    InterpolationCurve.CURVE)
+playerSprite = sprites.create(assets.image("""
+    player_ship
+"""), SpriteKind.player)
+weaponCooldown = 0
+weapon_1 = 0
+weapon_2 = 0
+playerWeapon = weapon_1
+playerSprite.set_position(80, 100)
+controller.move_sprite(playerSprite, 100, 100)
+ui_base = sprites.create(assets.image("""
+    ui
+"""), SpriteKind.UI)
+ui_missileDigitOne = sprites.create(assets.image("""
+    ui_digit_0
+"""), SpriteKind.UI)
+ui_missileDigitTen = sprites.create(assets.image("""
+    ui_digit_0
+"""), SpriteKind.UI)
+digits = [sprites.create(assets.image("""
+        ui_digit_0
+    """), SpriteKind.UI),
+    sprites.create(assets.image("""
+        ui_digit_1
+    """), SpriteKind.UI),
+    sprites.create(assets.image("""
+        ui_digit_2
+    """), SpriteKind.UI),
+    sprites.create(assets.image("""
+        ui_digit_3
+    """), SpriteKind.UI),
+    sprites.create(assets.image("""
+        ui_digit_4
+    """), SpriteKind.UI),
+    sprites.create(assets.image("""
+        ui_digit_5
+    """), SpriteKind.UI),
+    sprites.create(assets.image("""
+        ui_digit_6
+    """), SpriteKind.UI),
+    sprites.create(assets.image("""
+        ui_digit_7
+    """), SpriteKind.UI),
+    sprites.create(assets.image("""
+        ui_digit_8
+    """), SpriteKind.UI),
+    sprites.create(assets.image("""
+        ui_digit_9
+    """), SpriteKind.UI)]
+ui_missileDigitTen.set_position(7, 9)
+ui_missileDigitOne.set_position(13, 9)
+
+def on_on_update():
+    global weaponCooldown, proj_basic, proj_trishot, proj_missile, proj_plasma
+    if playerWeapon == 0 and (weaponCooldown == 0 and controller.A.is_pressed()):
+        weaponCooldown = 0.2
+        proj_basic = sprites.create_projectile_from_sprite(assets.image("""
+            proj_basic
+        """), playerSprite, 0, -250)
+        music.play(sfx_shoot_basic, music.PlaybackMode.IN_BACKGROUND)
+    if playerWeapon == 2 and (weaponCooldown == 0 and controller.A.is_pressed()):
+        weaponCooldown = 0.3
+        proj_trishot = sprites.create_projectile_from_sprite(assets.image("""
+                proj_trishot
+            """),
+            playerSprite,
+            50,
+            -250)
+        proj_trishot = sprites.create_projectile_from_sprite(assets.image("""
+                proj_trishot
+            """),
+            playerSprite,
+            0,
+            -250)
+        proj_trishot = sprites.create_projectile_from_sprite(assets.image("""
+                proj_trishot
+            """),
+            playerSprite,
+            -50,
+            -250)
+        music.play(sfx_shoot_basic, music.PlaybackMode.IN_BACKGROUND)
+    if playerWeapon == 1 and (weaponCooldown == 0 and controller.A.is_pressed()):
+        weaponCooldown = 0.6
+        proj_missile = sprites.create_projectile_from_sprite(assets.image("""
+            myImage
+        """), playerSprite, 0, -250)
+        music.play(sfx_shoot_missile, music.PlaybackMode.IN_BACKGROUND)
+    if playerWeapon == 3 and (weaponCooldown == 0 and controller.A.is_pressed()):
+        weaponCooldown = 1.5
+        proj_plasma = sprites.create_projectile_from_sprite(assets.image("""
+            proj_plasma
+        """), playerSprite, 0, -250)
+        music.play(sfx_shoot_plasma, music.PlaybackMode.IN_BACKGROUND)
+game.on_update(on_on_update)
+
+def on_on_update2():
+    if playerSprite.x < 9:
+        playerSprite.x = 9
+    if playerSprite.x > 152:
+        playerSprite.x = 152
+    if playerSprite.y < 7:
+        playerSprite.y = 7
+    if playerSprite.y > 114:
+        playerSprite.y = 114
+game.on_update(on_on_update2)
+
+def on_on_update3():
+    global ui_missileDigitOne
+    list2: List[Sprite] = []
+    ui_missileDigitOne = list2[0]
+game.on_update(on_on_update3)
+
+def on_update_interval():
+    global weaponCooldown
+    if weaponCooldown < 0.1 and weaponCooldown != 0:
+        weaponCooldown = 0
+    if weaponCooldown > 0:
+        weaponCooldown += -0.1
+game.on_update_interval(100, on_update_interval)
--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+namespace SpriteKind {
+    export const UI = SpriteKind.create()
+    export const enemy_Proj = SpriteKind.create()
+}
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     if (weapon_1 == playerWeapon) {
         playerWeapon = weapon_2
@@ -7,6 +11,17 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
         currentWeapon = 1
     }
 })
+sprites.onOverlap(SpriteKind.enemy_Proj, SpriteKind.Player, function (sprite, otherSprite) {
+    sprites.destroy(sprite)
+    if (info.life() < 0) {
+        playLongSFX(lsfx_player_hit, 0)
+        info.changeLifeBy(-1)
+    } else {
+        sprites.destroy(otherSprite, effects.disintegrate, 500)
+        music.play(sfx_death, music.PlaybackMode.UntilDone)
+        game.gameOver(false)
+    }
+})
 // This function takes a long sound effect and plays it
 function playLongSFX (lsfx: music.Playable[], delay: number) {
     for (let sound of lsfx) {
@@ -14,9 +29,12 @@ function playLongSFX (lsfx: music.Playable[], delay: number) {
         pause(delay)
     }
 }
-sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
-	
-})
+function updateMissileDisplay () {
+    let ui_missileDigitOne: Sprite = null
+    let ui_missileDigitTen: Sprite = null
+    ui_missileDigitTen.setPosition(7, 9)
+    ui_missileDigitOne.setPosition(13, 9)
+}
 let proj_plasma: Sprite = null
 let proj_missile: Sprite = null
 let proj_trishot: Sprite = null
@@ -25,11 +43,13 @@ let currentWeapon = 0
 let playerWeapon = 0
 let weapon_2 = 0
 let weapon_1 = 0
+let sfx_death: music.SoundEffect = null
+let lsfx_player_hit: music.SoundEffect[] = []
 effects.starField.startScreenEffect()
 // sound for weaker hits
 let lsfx_basic_hit = [music.createSoundEffect(WaveShape.Noise, 3856, 1, 255, 255, 30, SoundExpressionEffect.Tremolo, InterpolationCurve.Linear), music.createSoundEffect(WaveShape.Noise, 1984, 1627, 148, 81, 100, SoundExpressionEffect.Warble, InterpolationCurve.Logarithmic)]
 // sound for player hit
-let lsfx_player_hit = [music.createSoundEffect(WaveShape.Noise, 1627, 3232, 255, 255, 300, SoundExpressionEffect.Tremolo, InterpolationCurve.Linear), music.createSoundEffect(WaveShape.Noise, 1627, 3232, 255, 255, 300, SoundExpressionEffect.Tremolo, InterpolationCurve.Linear), music.createSoundEffect(WaveShape.Noise, 1627, 3232, 255, 255, 300, SoundExpressionEffect.Tremolo, InterpolationCurve.Linear)]
+lsfx_player_hit = [music.createSoundEffect(WaveShape.Noise, 1627, 3232, 255, 255, 300, SoundExpressionEffect.Tremolo, InterpolationCurve.Linear), music.createSoundEffect(WaveShape.Noise, 1627, 3232, 255, 255, 300, SoundExpressionEffect.Tremolo, InterpolationCurve.Linear), music.createSoundEffect(WaveShape.Noise, 1627, 3232, 255, 255, 300, SoundExpressionEffect.Tremolo, InterpolationCurve.Linear)]
 // sound for basic shot
 let sfx_shoot_basic = music.createSoundEffect(WaveShape.Square, 3648, 300, 255, 129, 150, SoundExpressionEffect.None, InterpolationCurve.Logarithmic)
 // sound for missile shot
@@ -37,18 +57,21 @@ let sfx_shoot_missile = music.createSoundEffect(WaveShape.Noise, 2430, 958, 255,
 // sound for missile shot
 let sfx_shoot_plasma = music.createSoundEffect(WaveShape.Square, 1463, 1, 255, 50, 400, SoundExpressionEffect.Warble, InterpolationCurve.Curve)
 // sound for missile shot
-let sfx_death = music.createSoundEffect(WaveShape.Square, 4299, 1137, 255, 50, 1000, SoundExpressionEffect.Warble, InterpolationCurve.Curve)
+sfx_death = music.createSoundEffect(WaveShape.Square, 4299, 1137, 255, 50, 1000, SoundExpressionEffect.Warble, InterpolationCurve.Curve)
 // sound for player heal
 let sfx_heal = music.createSoundEffect(WaveShape.Sine, 1, 3812, 255, 113, 200, SoundExpressionEffect.None, InterpolationCurve.Curve)
 let sfx_plasma_explosion = music.createSoundEffect(WaveShape.Square, 1463, 1, 255, 17, 500, SoundExpressionEffect.Warble, InterpolationCurve.Curve)
 let sfx_missile_explosion = music.createSoundEffect(WaveShape.Noise, 1538, 379, 255, 0, 250, SoundExpressionEffect.Tremolo, InterpolationCurve.Curve)
 let playerSprite = sprites.create(assets.image`player_ship`, SpriteKind.Player)
 let weaponCooldown = 0
+let missile_count = 15
 weapon_1 = 0
-weapon_2 = 0
+weapon_2 = 1
 playerWeapon = weapon_1
 playerSprite.setPosition(80, 100)
 controller.moveSprite(playerSprite, 100, 100)
+let ui_base = sprites.create(assets.image`ui`, SpriteKind.UI)
+updateMissileDisplay()
 game.onUpdate(function () {
     if (playerWeapon == 0 && (weaponCooldown == 0 && controller.A.isPressed())) {
         weaponCooldown = 0.2
@@ -62,10 +85,12 @@ game.onUpdate(function () {
         proj_trishot = sprites.createProjectileFromSprite(assets.image`proj_trishot`, playerSprite, -50, -250)
         music.play(sfx_shoot_basic, music.PlaybackMode.InBackground)
     }
-    if (playerWeapon == 1 && (weaponCooldown == 0 && controller.A.isPressed())) {
+    if (playerWeapon == 1 && (missile_count > 0 && (weaponCooldown == 0 && controller.A.isPressed()))) {
         weaponCooldown = 0.6
         proj_missile = sprites.createProjectileFromSprite(assets.image`myImage`, playerSprite, 0, -250)
         music.play(sfx_shoot_missile, music.PlaybackMode.InBackground)
+        missile_count += -1
+        updateMissileDisplay()
     }
     if (playerWeapon == 3 && (weaponCooldown == 0 && controller.A.isPressed())) {
         weaponCooldown = 1.5
